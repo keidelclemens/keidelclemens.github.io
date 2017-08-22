51591dd8f41f817b46f660229738ea04ffddde25
keidelclemens <31043413+keidelclemens@users.noreply.github.com>
2017-08-22 11:41:18 -0400

    Update search.js

--- resources/js/search.js
+++ resources/js/search.js
@@ -34,7 +34,7 @@
   console.log(searchTerm);
 
   if (searchTerm) {
-    document.getElementById('search-box').setAttribute("value", searchTerm);
+    document.getElementById('search_box').setAttribute("value", searchTerm);
 
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
